c3af526f6277e3ce17310155f9503adf99c2dc4e
Dzmitry Skavardka <skovdimka@gmail.com>
2026-03-04 12:38:21 +0100

    add title/subtitle

--- force-app/main/default/lwc/banner/banner.js
+++ force-app/main/default/lwc/banner/banner.js
@@ -1,3 +1,28 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class Banner extends LightningElement {}
+export default class Banner extends LightningElement {
+
+    @api title = null;
+    @api subtitle = null;
+
+    @api showTitle = false;
+    @api showSubtitle = false;
+
+    @api backgroundColor = null;
+    
+    @api backgroundImage = null;
+    @api showBackgroundImage = false;
+
+    get bannerStyle() {
+        return `background-color:${this.backgroundColor || '#f4f6f9'};`;
+    }
+
+    get getShowTitle() {
+        return this.showTitle === true && this.title;
+    }
+
+    get getShowSubtitle() {
+        return this.showSubtitle === true && this.subtitle;
+    }
+
+}
